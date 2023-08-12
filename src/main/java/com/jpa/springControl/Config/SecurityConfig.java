b81605f8e979125c7bc4321ee3be431fafb19894
btjdghks2 <btjdghks2@gmail.com>
2023-08-12 15:56:03 +0900

    security cors

--- src/main/java/com/jpa/springControl/Config/SecurityConfig.java
+++ src/main/java/com/jpa/springControl/Config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.jpa.springControl.Config;public class securityConfig {
+package com.jpa.springControl.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                        .anyRequest().authenticated();
+
+        http
+                .csrf()
+                .disable();
+    }
 }
